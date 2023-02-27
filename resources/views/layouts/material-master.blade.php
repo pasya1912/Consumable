a86647d25fb90c76543e67d181352a8f3f701605
Handika <handika@aiia.co.id>
2023-02-28 01:37:48 +0700

    dev-1.0.0 : add padding datatable

--- resources/views/layouts/material-master.blade.php
+++ resources/views/layouts/material-master.blade.php
@@ -10,7 +10,7 @@
 </div>
 <div class="row">
     <div class="col-lg-12">
-        <div class="card">
+        <div class="card" style="padding: 2rem;">
             <div class="card-datatable table-responsive">
                 <table class="datatables-basic table border-top">
                     <thead>
